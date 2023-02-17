43301145a798c265f15beb31eba66bb362474ed5
aydanO <91083277+aydanO@users.noreply.github.com>
2023-02-17 16:55:12 -0500

    fixed the bug

--- Lab6/earthQuake.py
+++ Lab6/earthQuake.py
@@ -1,31 +1,30 @@
 import urllib.request
 import json
 import csv
+import ssl
 
-url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson"
+context = ssl.create_default_context()
+context.check_hostname = False
+context.verify_mode = ssl.CERT_NONE
 
-with urllib.request.urlopen(url) as url:
-    data = json.loads(url.read().decode())
+with urllib.request.urlopen("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson", context=context) as response:
+    data = response.read()
 
-features = data["features"]
-turkey_syria_quakes = [quake for quake in features if "Turkey" in quake["properties"]["place"] or "Syria" in quake["properties"]["place"]]
+earthquakes = []
 
-with open("turkey_syria_quakes.csv", mode="w", newline="") as csv_file:
-    fieldnames = ["time", "place", "mag", "magType", "latitude", "longitude", "depth"]
-    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+for feature in json.loads(data)['features']:
+    place = feature['properties']['place']
+    if "Turkey" in place or "Syria" in place:
+        time = feature['properties']['time']
+        mag = feature['properties']['mag']
+        magType = feature['properties']['magType']
+        latitude = feature['geometry']['coordinates'][1]
+        longitude = feature['geometry']['coordinates'][0]
+        depth = feature['geometry']['coordinates'][2]
+        earthquakes.append((time, place, mag, magType, latitude, longitude, depth))
 
-    writer.writeheader()
+with open('turkey_syria_earthquakes.csv', mode='w', newline='', encoding='utf-8') as csv_file:
+    writer = csv.writer(csv_file)
+    writer.writerow(['time', 'place', 'mag', 'magType', 'latitude', 'longitude', 'depth'])
+    writer.writerows(earthquakes)
 
-    for quake in turkey_syria_quakes:
-        properties = quake["properties"]
-        writer.writerow({
-            "time": properties["time"],
-            "place": properties["place"],
-            "mag": properties["mag"],
-            "magType": properties["magType"],
-            "latitude": quake["geometry"]["coordinates"][1],
-            "longitude": quake["geometry"]["coordinates"][0],
-            "depth": quake["geometry"]["coordinates"][2]
-
-
-        })
